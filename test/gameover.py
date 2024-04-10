baa315d86c402e3a7afe387e7379e2ce29fbc1cf
Ryan <128712516+wbsdsb@users.noreply.github.com>
2024-04-10 15:42:36 +0800

    Add files via upload
    将各page/游戏模式导入游戏，目前可以正常运行，待解决
    1.遇到bug,私信hrx
    2.没有导入音乐
    3.各种游戏模式
    4.优化页面
    5.删除test函数

---
+++ test/gameover.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import cocos
+from cocos.director import director
+import define
+
+class Gameover(cocos.layer.ColorLayer):
+    def __init__(self):
+        window_width, window_height = cocos.director.director.get_window_size()
+        # background_width, background_height = window_width // 2, window_height // 2
+
+        super(Gameover, self).__init__(200, 235, 235, 200, width=window_width, height=window_height)
+
+        # position_x = 0
+        # position_y = 0
+        # self.position = position_x, position_y
+
+        self.visible = False
+        self.score = cocos.text.Label('',
+                                      font_name='SimHei',
+                                      font_size=72,
+                                      color=define.MAROON)
+        self.score.position = window_width // 2, window_height // 2 + window_height // 16
+        self.add(self.score)
+
+        text = cocos.text.Label('score: ',
+                                font_name='SimHei',
+                                font_size=72,
+                                color=define.MAROON)
+        text.position = window_width // 2 - window_width // 8, window_height // 2 + window_height // 16
+        self.add(text)
+        text = cocos.text.Label('   J or Click    -    Restart Game',
+                                font_name='SimHei',
+                                font_size=48,
+                                color=define.MAROON)
+        text.position = window_width // 2 - window_width // 4, window_height // 2 - window_height // 16
+        self.add(text)
+        text = cocos.text.Label('BACKSPACE - Back to Homepage',
+                                font_name='SimHei',
+                                font_size=48,
+                                color=define.MAROON)
+        text.position = window_width // 2 - window_width // 4, window_height // 2 - window_height // 8
+        self.add(text)
